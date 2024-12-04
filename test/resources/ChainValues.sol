e24a51ed050a4560c66901ed89304ca0ff56a2ca
mel0ndev <motherfker.jones1@gmail.com>
2024-12-03 19:36:15 -0500

    btcn mainnet script

--- test/resources/ChainValues.sol
+++ test/resources/ChainValues.sol
@@ -186,6 +186,7 @@ contract ChainValues {
         values[mainnet]["eBTC"] = 0x657e8C867D8B37dCC18fA4Caead9C45EB088C642.toBytes32();
         values[mainnet]["USDS"] = 0xdC035D45d973E3EC169d2276DDab16f1e407384F.toBytes32();
         values[mainnet]["uniBTC"] = 0x004E9C3EF86bc1ca1f0bB5C7662861Ee93350568.toBytes32();
+        values[mainnet]["BTCN"] = 0x386E7A3a0c0919c9d53c3b04FF67E73Ff9e45Fb6.toBytes32();
 
         // Rate providers
         values[mainnet]["WEETH_RATE_PROVIDER"] = 0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee.toBytes32();
