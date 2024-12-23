e7e42de0bd1f00e0bafa3b6cdc2a2079095a370f
mel0ndev <motherfker.jones1@gmail.com>
2024-12-23 10:06:37 -0500

    add native wrapping/unwrapping corn decoder

--- script/MerkleRootCreation/Corn/CreateStakedBTCNMerkleRoot.s.sol
+++ script/MerkleRootCreation/Corn/CreateStakedBTCNMerkleRoot.s.sol
@@ -17,7 +17,7 @@ contract CreateStakedBTCNMerkleRoot is Script, MerkleTreeHelper {
     address boringVault = 0x5E272ca4bD94e57Ec5C51D26703621Ccac1A7089; 
     address managerAddress = 0x5239158272D1f626aF9ef3353489D3Cb68439D66;
     address accountantAddress = 0x9A22F5dC4Ec86184D4771E620eb75D52E7b9E043; 
-    address rawDataDecoderAndSanitizer = 0xCAc92301f96e3b6554EF11366482f464c6f87cFB; 
+    address rawDataDecoderAndSanitizer = 0x284b1B0Cc7C430e3F1eb11A37836fe61157c19CD; 
 
     function run() external {
         /// NOTE Only have 1 function run at a time, otherwise the merkle root created will be wrong.
@@ -32,7 +32,6 @@ contract CreateStakedBTCNMerkleRoot is Script, MerkleTreeHelper {
         setAddress(false, corn, "rawDataDecoderAndSanitizer", rawDataDecoderAndSanitizer);
 
         ManageLeaf[] memory leafs = new ManageLeaf[](16);
-
         // ========================== Curve ==========================
         _addCurveLeafs(leafs, getAddress(sourceChain, "curve_pool_LBTC_WBTCN"), 2, getAddress(sourceChain, "curve_gauge_LBTC_WBTCN")); 
 
@@ -43,6 +42,11 @@ contract CreateStakedBTCNMerkleRoot is Script, MerkleTreeHelper {
         _addLayerZeroLeafs(
             leafs, getERC20(sourceChain, "LBTC"), getAddress(sourceChain, "LBTC_OFT"), layerZeroMainnetEndpointId
         ); 
+
+        // ========================== Native Wrapping ==========================
+        _addNativeLeafs(leafs, getAddress(sourceChain, "WBTCN")); 
+
+
 
         _verifyDecoderImplementsLeafsFunctionSelectors(leafs);
 
