f034d7a85bb49e10a59eb56b46f3e5a5cef0a4ae
mel0ndev <motherfker.jones1@gmail.com>
2025-03-11 14:04:41 -0400

    wip

---
+++ src/base/DecodersAndSanitizers/Protocols/BalancerV3DecoderAndSanitizer.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {BaseDecoderAndSanitizer, DecoderCustomTypes} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+
+abstract contract BalancerV2DecoderAndSanitizer is BaseDecoderAndSanitizer {
+    //============================== ERRORS ===============================
+    
+    // Router 
+    // Add Liquidity
+    // Remove Liquidity
+    // Swaps   
+    
+}
