1770c32b1baf4e05fa3370ec0131a6b17c498d28
mel0ndev <motherfker.jones1@gmail.com>
2025-03-25 09:30:04 -0400

    wip

---
+++ src/base/DecodersAndSanitizers/Protocols/DeriveDecoderAndSanitizer.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {INonFungiblePositionManager} from "src/interfaces/RawDataDecoderAndSanitizerInterfaces.sol";
+import {BaseDecoderAndSanitizer, DecoderCustomTypes} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+
+abstract contract DeriveDecoderAndSanitizer is BaseDecoderAndSanitizer {
+    //============================== ERRORS ===============================
+    
+    
+    
+}
