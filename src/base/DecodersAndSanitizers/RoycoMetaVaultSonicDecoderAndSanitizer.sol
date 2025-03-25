f6dc72e68c7d68fe52047eee7b5fcddf8a5e5a67
Eidolon <92181746+imrtlfarm@users.noreply.github.com>
2025-03-24 17:23:28 -0700

    Add 3 Royco Big Decoders
    MetaVault Sonic decoder is incomplete and needs to be updated once boringchef decoder is completed

---
+++ src/base/DecodersAndSanitizers/RoycoMetaVaultSonicDecoderAndSanitizer.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {BaseDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+import {SonicGatewayDecoderAndSanitizer} from
+    "src/base/DecodersAndSanitizers/Protocols/SonicGatewayDecoderAndSanitizer.sol";
+import {OFTDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/OFTDecoderAndSanitizer.sol";
+import {OdosDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/OdosDecoderAndSanitizer.sol";
+import {TellerDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/TellerDecoderAndSanitizer.sol";
+//import {BoringChefDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/BoringChefDecoderAndSanitizer.sol";
+// TODO: import BoringChefDecoderAndSanitizer after it is made
+
+// TODO: inherit from BoringChefDecoderAndSanitizer after it is made
+contract RoycoSonicNativeDecoderAndSanitizer is
+    BaseDecoderAndSanitizer,
+    SonicGatewayDecoderAndSanitizer,
+    OFTDecoderAndSanitizer,
+    OdosDecoderAndSanitizer,
+    TellerDecoderAndSanitizer
+{
+    constructor(address _odosRouter)
+        BaseDecoderAndSanitizer()
+        SonicGatewayDecoderAndSanitizer()
+        OFTDecoderAndSanitizer()
+        OdosDecoderAndSanitizer(_odosRouter)
+        TellerDecoderAndSanitizer()
+    {}
+}
