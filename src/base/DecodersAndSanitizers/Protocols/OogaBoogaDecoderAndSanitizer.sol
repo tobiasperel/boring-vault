1d1af4d49c3a733e84fdea80d841ac6b0c0462ca
mel0ndev <motherfker.jones1@gmail.com>
2025-03-28 13:17:08 -0400

    add ooga booga and pol tests

---
+++ src/base/DecodersAndSanitizers/Protocols/OogaBoogaDecoderAndSanitizer.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {BaseDecoderAndSanitizer, DecoderCustomTypes} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+
+
+abstract contract OogaBoogaDecoderAndSanitizer is BaseDecoderAndSanitizer {
+
+    function swap(
+        DecoderCustomTypes.swapTokenInfoOogaBooga memory tokenInfo,
+        bytes calldata /*pathDefinition*/,
+        address executor,
+        uint32 /*referralCode*/
+    ) external pure virtual returns (bytes memory addressesFound) {
+        addressesFound = abi.encodePacked(tokenInfo.inputToken, tokenInfo.outputToken, tokenInfo.outputReceiver, executor); 
+    }
+}
