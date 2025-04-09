be3850f8a410e1628fbf354dae872928f7b00488
Eidolon <92181746+imrtlfarm@users.noreply.github.com>
2025-04-08 18:15:59 -0700

    Update TacLBTCvDecoderAndSanitizer.sol

--- src/base/DecodersAndSanitizers/TacLBTCvDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/TacLBTCvDecoderAndSanitizer.sol
@@ -5,16 +5,17 @@ import {BaseDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/BaseDecode
 import {UniswapV3DecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/UniswapV3DecoderAndSanitizer.sol";
 import {OdosDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/OdosDecoderAndSanitizer.sol";
 import {OneInchDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/OneInchDecoderAndSanitizer.sol";
+import {TellerDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/TellerDecoderAndSanitizer.sol";
 
 contract TacLBTCvDecoderAndSanitizer is
     BaseDecoderAndSanitizer,
     UniswapV3DecoderAndSanitizer,
     OdosDecoderAndSanitizer,
-    OneInchDecoderAndSanitizer
+    OneInchDecoderAndSanitizer,
+    TellerDecoderAndSanitizer
 {
     constructor(address _uniswapV3NonFungiblePositionManager, address _odosRouter)
         UniswapV3DecoderAndSanitizer(_uniswapV3NonFungiblePositionManager)
         OdosDecoderAndSanitizer(_odosRouter)
-        OneInchDecoderAndSanitizer()
     {}
 }
