b168eccc860f04d49e04da0b01526b47fb6191bc
mel0ndev <motherfker.jones1@gmail.com>
2025-04-22 10:07:21 -0400

    wip

---
+++ src/base/DecodersAndSanitizers/Protocols/dvStETHDecoderAndSanitizer.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {BaseDecoderAndSanitizer, DecoderCustomTypes} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+
+
+abstract contract DvStETHDecoderAndSanitizer is BaseDecoderAndSanitizer {
+
+
+    function deposit(
+        address to,
+        uint256[] memory /*amounts*/,
+        uint256 /*minLpAmount*/,
+        uint256 /*deadline*/,
+        uint256 /*referralCode*/
+    ) external pure virtual returns (bytes memory addressesFound) {
+        addressesFound = abi.encodePacked(to); 
+    }
+
+    function emergencyWithdraw(uint256[] memory /*minAmounts*/, uint256 /*deadline*/) external pure virtual returns (bytes memory addressesFound) {
+        return addressesFound; 
+    }
+
+    function registerWithdrawal(
+        address to,
+        uint256 /*lpAmount*/,
+        uint256[] memory /*minAmounts*/,
+        uint256 /*deadline*/,
+        uint256 /*requestDeadline*/,
+        bool /*closePrevious*/
+    ) external pure virtual returns (bytes memory addressesFound) {
+        addressesFound = abi.encodePacked(to); 
+    }
+
+    function cancelWithdrawalRequest() external pure virtual returns (bytes memory addressesFound) {
+        return addressesFound; 
+    }
+
+}
+
---
+++ test/integrations/DvStETHIntegration.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {BaseTestIntegration} from "test/integrations/BaseTestIntegration.t.sol"; 
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+import {DvStETHDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/DvStETHDecoderAndSanitizer.sol"; 
+import {DecoderCustomTypes} from "src/interfaces/DecoderCustomTypes.sol"; 
+import {ERC4626DecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/ERC4626DecoderAndSanitizer.sol";
+import {CurveDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/CurveDecoderAndSanitizer.sol"; 
+import {Test, stdStorage, StdStorage, stdError, console} from "@forge-std/Test.sol";
+
+contract FullDvStETHDecoderAndSanitizer is DvStETHDecoderAndSanitizer {
+
+
+}
