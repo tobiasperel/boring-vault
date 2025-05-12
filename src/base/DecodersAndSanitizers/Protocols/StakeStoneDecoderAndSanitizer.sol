8874fb021f199a9f8b1efafbde2e2d9e52647611
Eidolon <92181746+imrtlfarm@users.noreply.github.com>
2025-05-12 11:45:32 -0700

    add stakestone decoder

---
+++ src/base/DecodersAndSanitizers/Protocols/StakeStoneDecoderAndSanitizer.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {BaseDecoderAndSanitizer, DecoderCustomTypes} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+
+abstract contract SwellDecoderAndSanitizer is BaseDecoderAndSanitizer {
+    //============================== StoneVault ===============================
+
+    function deposit() external pure virtual returns (bytes memory addressesFound) {
+        // Nothing to sanitize or return
+        return addressesFound;
+    }
+
+    function instantWithdraw(uint256 /*_amount*/, uint256 /*_shares*/) external pure virtual returns (bytes memory addressesFound) {
+        // Nothing to sanitize or return
+        return addressesFound;
+    }
+
+    function requestWithdraw(uint256 /*_shares*/) external pure virtual returns (bytes memory addressesFound) {
+        // Nothing to sanitize or return
+        return addressesFound;
+    }
+}
