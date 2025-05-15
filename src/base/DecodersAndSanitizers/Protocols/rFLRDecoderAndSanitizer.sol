9c438a6caded0dcdc416ffb228a5427725fe7cfa
Eidolon <92181746+imrtlfarm@users.noreply.github.com>
2025-05-15 11:08:33 -0700

    add rFLR decoder

---
+++ src/base/DecodersAndSanitizers/Protocols/rFLRDecoderAndSanitizer.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {BaseDecoderAndSanitizer, DecoderCustomTypes} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
+
+abstract contract rFLRDecoderAndSanitizer is BaseDecoderAndSanitizer {
+    //============================== rFLR ===============================
+    function claimRewards(uint256[] calldata /*_projectIds*/, uint256 /*_month*/) external pure virtual returns (bytes memory addressesFound) {
+        return addressesFound;
+    }
+
+    function withdraw(uint128 /*_amount*/, bool /*_wrap*/) external pure virtual returns (bytes memory addressesFound) {
+        return addressesFound;
+    }
+
+    function withdrawAll(bool /*_wrap*/) external pure virtual returns (bytes memory addressesFound) {
+        return addressesFound;
+    }
+}
