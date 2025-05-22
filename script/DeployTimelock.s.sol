81e56e8fb7ae80df86fbeb23e7892317f77b1996
Eidolon <92181746+imrtlfarm@users.noreply.github.com>
2025-05-22 16:41:37 -0700

    deploy timelocks for golden goose

--- script/DeployTimelock.s.sol
+++ script/DeployTimelock.s.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.21;
 
 import {Deployer} from "src/helper/Deployer.sol";
-import {RolesAuthority, Authority} from "@solmate/auth/authorities/RolesAuthority.sol";
 import {ContractNames} from "resources/ContractNames.sol";
 import {MainnetAddresses} from "test/resources/MainnetAddresses.sol";
 import {TimelockController, AccessControl} from "@openzeppelin/contracts/governance/TimelockController.sol";
@@ -16,13 +15,10 @@ import "forge-std/StdJson.sol";
 contract DeployTimelockScript is Script, ContractNames, MainnetAddresses {
     uint256 public privateKey;
 
-    // Contracts to deploy
-    RolesAuthority public rolesAuthority;
     Deployer public deployer;
     TimelockController public timelock;
 
     address public canceller = 0xA916fD5252160A7E56A6405741De76dc0Da5A0Cd;
-    address public proposer = 0xD48b7e87fDCCaCa7ea93F347755c799eBE0fD35F;
     address public executor = address(0);
 
     function setUp() external {
@@ -38,18 +34,22 @@ contract DeployTimelockScript is Script, ContractNames, MainnetAddresses {
         deployer = Deployer(deployerAddress);
         creationCode = type(TimelockController).creationCode;
         uint256 minDelay = 0;
-        address[] memory proposers = new address[](1);
-        proposers[0] = proposer;
+        address[] memory proposers = new address[](3);
+        proposers[0] = 0xD48b7e87fDCCaCa7ea93F347755c799eBE0fD35F; // real
+        proposers[1] = 0xf8553c8552f906C19286F21711721E206EE4909E; // temp
+        proposers[2] = 0x1cdF47387358A1733968df92f7cC14546D9E1047; // temp
         address[] memory executors = new address[](1);
         executors[0] = executor;
         address tempAdmin = 0x7E97CaFdd8772706dbC3c83d36322f7BfC0f63C7;
         constructorArgs = abi.encode(minDelay, proposers, executors, tempAdmin);
         timelock =
-        TimelockController(payable(deployer.deployContract("alphaSTETH Timelock V0.1", creationCode, constructorArgs, 0)));
+        TimelockController(payable(deployer.deployContract("Golden Goose Timelock V0.1", creationCode, constructorArgs, 0)));
 
 
         timelock.grantRole(timelock.CANCELLER_ROLE(), canceller);
-        timelock.revokeRole(timelock.CANCELLER_ROLE(), proposer);
+        for (uint256 i = 0; i < proposers.length; i++) {
+            timelock.revokeRole(timelock.CANCELLER_ROLE(), proposers[i]);
+        }
         timelock.renounceRole(timelock.DEFAULT_ADMIN_ROLE(), tempAdmin);
 
         vm.stopBroadcast();
