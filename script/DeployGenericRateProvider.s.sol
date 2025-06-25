4a4170299f88034e90c3b53c097dfab04d9da961
mel0ndev <motherfker.jones1@gmail.com>
2025-06-24 22:22:16 -0400

    not deployed yet

--- script/DeployGenericRateProvider.s.sol
+++ script/DeployGenericRateProvider.s.sol
@@ -18,13 +18,13 @@ import "forge-std/Test.sol";
 contract DeployGenericRateProvider is Script, ContractNames, Test {
     uint256 public privateKey;
     
-    address target = 0x65eD6a4ac085620eE943c0B15525C4428D23e4Db; 
-    bytes4 selector = 0x50d25bcd; 
+    address target = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0; 
+    bytes4 selector = 0x9576a0c8; 
     Deployer deployer = Deployer(0x5F2F11ad8656439d5C14d9B351f8b09cDaC2A02d); 
 
     function setUp() external {
         privateKey = vm.envUint("BORING_DEVELOPER");
-        vm.createSelectFork("berachain");
+        vm.createSelectFork("mainnet");
     }
 
     function run() external {
@@ -42,7 +42,7 @@ contract DeployGenericRateProvider is Script, ContractNames, Test {
             0, 0,
             true
         );
-        address createdAddress = deployer.deployContract("WeETH Rate Provider V0.0", creationCode, constructorArgs, 0); 
+        address createdAddress = deployer.deployContract("wstETH Rate Provider V0.1", creationCode, constructorArgs, 0); 
         console.log("DEPLOYED ADDRESS: ", createdAddress); 
         //require(createdAddress == 0x983dC32F0F022F1e114Bf54c280B3575A512BF4f, "not premined"); 
         //require(GenericRateProvider(createdAddress).getRate() == 1038891179797110067, "bad price"); 
